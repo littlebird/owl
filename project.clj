9c9d5491b577c689c013562aa05d679a7a315a60
Ryan Spangler <ryan@getlittlebird.com>
2015-04-16 15:04:33 -0700

    bumping version

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
-(defproject aviary/owl "0.0.4"
+(defproject aviary/owl "0.0.5"
   :description "Graph analysis in Clojure"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/littlebird/owl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.taoensso/timbre "3.3.1"]])
+                 [com.taoensso/timbre "3.3.1"]]
+  :plugins [[s3-wagon-private "1.1.2"]]
+  :repositories [["private" {:url "s3p://littlebird-maven/releases/"
+                             :creds :gpg
+                             :sign-releases false}]])
