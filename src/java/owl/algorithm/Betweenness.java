d7097c081919b0407eb0f6e62c684c9df4ec9e70
Justin Smith <noisesmith@gmail.com>
2016-12-18 18:42:54 -0800

    preparing to try an incremental algorithm here

--- src/java/owl/algorithm/Betweenness.java
+++ src/java/owl/algorithm/Betweenness.java
@@ -3,73 +3,81 @@ package owl.algorithm;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Stack;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.HashMap;
-import clojure.lang.IPersistentMap;
-import clojure.lang.MapEntry;
-import clojure.lang.Sequential;
 
 public class Betweenness {
-  private HashMap graph;
+  private Map graph;
 
-  public Betweenness(IPersistentMap graph) {
-    this.graph = new HashMap((Map) graph);
+  public Betweenness(Map graph) {
+    this.graph = graph;
   }
 
-  public Number get(HashMap map, Long key, Number base) {
-    return map.containsKey(key) ? (Number) map.get(key) : base;
+  public Long get(Map<Long, Long> map, Long key, Long base) {
+    return map.containsKey(key) ? map.get(key) : base;
+  }
+
+  public Double get(Map<Long, Double> map, Long key, Double base) {
+    return map.containsKey(key) ? map.get(key) : base;
   }
 
   public HashMap calculate() {
     int size = this.graph.size();
-    HashMap betweenness = new HashMap(size);
+    HashMap betweenness = new HashMap<Long, Double>(size);
 
-    for (Iterator keys = this.graph.keySet().iterator(); keys.hasNext();) {
-      Long node = (Long) keys.next();
+    for (Iterator keys = this.graph.keySet().iterator();
+         keys.hasNext();) {
+      long node = (long) keys.next();
 
-      Stack stack = new Stack();
-      HashMap paths = new HashMap(size);
-      HashMap shortest = new HashMap(size);
-      HashMap dependence = new HashMap(size);
-      HashMap distance = new HashMap(size);
-      LinkedList queue = new LinkedList();
-      shortest.put(node, 1);
-      dependence.put(node, 0);
+      ArrayDeque stack = new ArrayDeque<Long> ();
+      HashMap<Long, ArrayDeque<Long>> paths = new HashMap(size);
+      HashMap<Long, Long> shortest = new HashMap(size);
+      HashMap<Long, Long> dependence = new HashMap(size);
+      HashMap<Long, Double> distance = new HashMap(size);
+      ArrayDeque<Long> queue = new ArrayDeque();
+      shortest.put(node, 1l);
+      dependence.put(node, 0l);
       queue.add(node);
 
       while(queue.size() > 0) {
-        Long visit = (Long) queue.poll();
+        long visit = (long) queue.poll();
         stack.push(visit);
-        for(Iterator neighbors = ((Collection) this.graph.get(visit)).iterator(); neighbors.hasNext();) {
-          Long neighbor = (Long) neighbors.next();
+        Collection neighborList = (Collection) this.graph.get(visit);
+        for(Iterator neighbors = neighborList.iterator();
+            neighbors.hasNext();) {
+          long neighbor = (long) neighbors.next();
           if (!dependence.containsKey(neighbor)) {
             queue.add(neighbor);
-            dependence.put(neighbor, ((int) this.get(dependence, visit, -1)) + 1);
+            dependence.put(neighbor, this.get(dependence, visit, -1l) + 1);
           }
 
-          if ((int) dependence.get(neighbor) == ((int) this.get(dependence, visit, -1)) + 1) {
-            shortest.put(neighbor, ((int) this.get(shortest, neighbor, 0)) + ((int) this.get(shortest, visit, 0)));
+          if (dependence.get(neighbor) == this.get(dependence, visit, -1l) + 1) {
+            long soFar = this.get(shortest, neighbor, 0l);
+            long further = this.get(shortest, visit, 1l);
+            shortest.put(neighbor, soFar+further);
             if (!paths.containsKey(neighbor)) {
-              paths.put(neighbor, new LinkedList());
+              paths.put(neighbor, new ArrayDeque());
             }
-
-            ((LinkedList) paths.get(neighbor)).add(visit);
+            paths.get(neighbor).add(visit);
           }
         }
       }
 
-      while(!stack.empty()) {
-        Long last = (Long) stack.pop();
+      while(!stack.isEmpty()) {
+        long last = (long) stack.pop();
         if (paths.containsKey(last)) {
-          for (Iterator path = ((LinkedList) paths.get(last)).iterator(); path.hasNext();) {
-            Long step = (Long) path.next();
-            double ratio = ((int) this.get(shortest, step, 0)) / (double) ((int) shortest.get(last));
-            double scale = ((double) this.get(distance, last, 0.0)) + 1;
-            distance.put(step, ((double) this.get(distance, step, 0.0)) + ratio * scale);
-
+          ArrayDeque pathList = paths.get(last);
+          for (Iterator path = pathList.iterator();
+               path.hasNext();) {
+            long step = (long) path.next();
+            double stepDistance = this.get(distance, step, 0.0);
+            double ratio = this.get(shortest, step, 0l) / shortest.get(last);
+            double scale = this.get(distance, last, 0.0) + 1.0;
+            distance.put(step, stepDistance + ratio * scale);
             if (last != node) {
-              betweenness.put(last, ((double) this.get(betweenness, last, 0.0)) + ((double) get(distance, last, 0.0)));
+              double lastBetweenness = this.get(betweenness, last, 0.0);
+              double lastDistance = this.get(distance, last, 0.0);
+              betweenness.put(last,  lastBetweenness + lastDistance);
             }
           }
         }
@@ -79,5 +87,3 @@ public class Betweenness {
     return betweenness;
   }
 }
-
-
